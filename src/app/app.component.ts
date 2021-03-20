336c6944a91a51c648e2bff5bdcaa60fe06d0b25
J <juancarlospecan@gmail.com>
2021-03-20 16:27:10 +0100

    base01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './classes/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testWeekendOne';
+  public title: string = 'My First Week ';
+  public user: User = new User(20, 'lol', 'sda');
+
+  public users: User[] = [
+    new User(23, 'lola', 'Smith'),
+    new User(54, 'hector', 'Pator'),
+    new User(78, 'Waren', 'Amlet')
+  ];
+
+
+  public tab:number[] = [1,2,3,4,5]
+
+  public tab2:number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+  constructor(){
+
+
+  }
+
+
+  public helloWorld():string{
+    return 'hello world from the function helloWorld()'
+  }
+
+
+  public name:string = "Robin";
+  public color:string = 'red'
+  
+
+
+  // public displayBlue(): boolean {
+  //   return this.color === "blue" ? true : false;
+  // }
+
 }
---
+++ src/app/classes/user.ts
@@ -0,0 +1,37 @@
+export class User {
+
+    constructor(private _num?:number,private _firstname?:string,private _lastname?:string){
+        // this.num = num;
+        // this.firstname = firstname;
+        // this.lastname = lastname;
+    }
+
+
+    get num():number {
+        return this._num;
+    }
+    
+    set num(num:number){
+        this._num = num;
+    }
+
+    get firstname():string {
+        return this._firstname;
+    }
+
+    set firstname(firstname:string) {
+        this._firstname = firstname;
+    }
+
+    get lastname():string {
+        return this._lastname;
+    }
+    
+    set lastname(lastname:string){
+        this._lastname;
+    }
+}
+
+
+
+ 
